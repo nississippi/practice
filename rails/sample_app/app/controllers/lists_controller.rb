429791839cd0efbe624a8b9443156b2380dab682
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-07-03 11:45:25 +0000

    詳細画面に本文表示されないエラー解決

--- rails/sample_app/app/controllers/lists_controller.rb
+++ rails/sample_app/app/controllers/lists_controller.rb
@@ -2,7 +2,7 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
@@ -18,10 +18,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+    
   end
-  
+
   private
   def list_params
-    params.require(:list).permit(:title, :bo)
+    params.require(:list).permit(:title, :body)
   end
 end
